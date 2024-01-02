8ec370bb7d4df5ad89f234ce2bd2c51f2fd57221
Hrishikesh Barman <oss@geekodour.org>
2024-01-02 15:45:36 +0530

    chore: add pgx

--- cmd/smol/main.go
+++ cmd/smol/main.go
@@ -1,25 +1,62 @@
 package main
 
+// TODO:
+// 1. logging & error
+// 2. lifecycle
+// 3. http server
+// 4. sqlc + pgx
+// 5. add the api endpoints
+// 6. benchmark and stresstest
+
 import (
-	"log"
-	"net/http"
+	"context"
+	// "fmt"
+	"log/slog"
+	// "net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
-	mux := http.NewServeMux()
+func setupLogger() {
+	logLevel := &slog.LevelVar{} // INFO
+	logOpts := slog.HandlerOptions{Level: logLevel, AddSource: true}
 
-	// CRUD endpoints for cats
-	// mux.HandleFunc("/cats", getAllCats)
-	// mux.HandleFunc("/cats/add", addCat)
-	// mux.HandleFunc("/cats/update", updateCat)
-	// mux.HandleFunc("/cats/delete", deleteCat)
+	handler := func() slog.Handler {
+		if getEnv() == "production" {
+			return slog.NewJSONHandler(os.Stderr, &logOpts)
+		}
+		return slog.NewTextHandler(os.Stderr, &logOpts)
+	}()
+	logger := slog.New(handler)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	slog.SetDefault(logger)
+	// logLevel.Set(slog.LevelDebug) // if debug needed
+}
+
+func getEnv() string {
+	env, ok := os.LookupEnv("PROJECT_ENV")
+	if !ok {
+
+		slog.Error("unset env var")
+		os.Exit(1)
 	}
+	if env == "" {
+		env = "development"
+	}
+	return env
+}
 
-	log.Printf("Server listening on :%s...\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+func dbPool() *pgxpool.Pool {
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
+	if err != nil {
+		slog.Error("Unable to connect to database", slog.String("error", err.Error()))
+	}
+	return dbpool
+}
+
+func main() {
+	setupLogger()
+	dbpool := dbPool()
+	defer dbpool.Close()
 }
